d065346a4ef5ce22195b990517177c917f46ef6a
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2024-12-03 16:26:28 +0100

    add profiles table

---
+++ src/db/schema/profiles.ts
@@ -0,0 +1,22 @@
+import { pgTable, serial, varchar, boolean, integer, timestamp } from 'drizzle-orm/pg-core'
+import users from './users'
+import { relations } from 'drizzle-orm'
+
+const timestamps = {
+    updated_at: timestamp(),
+    created_at: timestamp().defaultNow().notNull(),
+    deleted_at: timestamp(),
+}
+
+const profiles = pgTable('profiles', {
+    id: serial('id').primaryKey(),
+    userId: integer('user_id').references(() => users.id).unique(),
+    currency: varchar('currency'),
+    ...timestamps
+})
+
+export const profilesRelations = relations(profiles, ({ one }) => ({
+    user: one(users, { fields: [profiles.userId], references: [users.id] })
+}))
+
+export default profiles
--- src/db/schema/users.ts
+++ src/db/schema/users.ts
@@ -1,12 +1,25 @@
-import { pgTable, serial, varchar, boolean } from 'drizzle-orm/pg-core'
+import { pgTable, serial, varchar, boolean, timestamp } from 'drizzle-orm/pg-core'
+import { relations } from 'drizzle-orm'
+import profiles from './profiles'
+
+const timestamps = {
+    updated_at: timestamp(),
+    created_at: timestamp().defaultNow().notNull(),
+    deleted_at: timestamp(),
+}
 
 const users = pgTable('users', {
     id: serial('id').primaryKey(),
-    firstName: varchar('first_name', { length: 255 }).notNull(),
-    lastName: varchar('last_name', { length: 255 }).notNull(),
-    email: varchar('email', { length: 255 }).notNull().unique(),
+    firstName: varchar('first_name', { length: 50 }).notNull(),
+    lastName: varchar('last_name', { length: 50 }).notNull(),
+    email: varchar('email', { length: 64 }).notNull().unique(),
     verified: boolean('verified').default(false),
-    password: varchar('password', { length: 255 }).notNull()
+    password: varchar('password', { length: 255 }).notNull(),
+    ...timestamps
 })
+
+export const usersRelations = relations(users, ({ one }) => ({
+    profile: one(profiles)
+}))
 
 export default users
