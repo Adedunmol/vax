dde8cb627d147f6188ac3d3ec23aa0d70ae6f8f0
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2024-12-10 19:56:15 +0100

    setup migration[still failing]

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,6 +2,8 @@ import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 import env from '../env'
 
+console.log("DB_URL", env.DB_URL)
+
 export const connection = postgres(env.DB_URL, {
     max: (env.DB_MIGRATING || env.DB_SEEDING) ? 1 : undefined,
     onnotice: env.DB_SEEDING ? () => {} : undefined
