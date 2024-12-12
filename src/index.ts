839cd5489d5a4764b170d134afb8952d6f83d8dc
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2024-12-12 20:36:12 +0100

    add bull board

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,32 @@
 import fastifyJwt from '@fastify/jwt'
 import fastifyRedis from '@fastify/redis'
+import { createBullBoard } from '@bull-board/api'
+import { BullMQAdapter } from '@bull-board/api/bullMQAdapter'
+import { FastifyAdapter } from '@bull-board/fastify'
 import buildServer from './app'
 import env from './env'
 import { errorSchemas } from './modules/errors/schema-base'
 import userRoutes from './modules/user/user.route'
 import { userSchemas } from './modules/user/user.schema'
 import { FastifyReply, FastifyRequest } from 'fastify'
+import { emailQueue } from './queues/email/producer'
 
 export const server = buildServer({
     logger: true
 })
 
+const serverAdapter = new FastifyAdapter()
+
+createBullBoard({
+    queues: [new BullMQAdapter(emailQueue)],
+    serverAdapter
+})
+
 server.register(fastifyJwt, { secret: env.JWT_SECRET })
 server.register(fastifyRedis, { host: env.REDIS_HOST, port: env.REDIS_PORT, password: env.REDIS_PASSWORD })
+server.register(serverAdapter.registerPlugin(), { prefix: '/bull-board', basePath: '/bull-board' })
 
-server.decorate('authenticate', async (request: FastifyRequest, reply :FastifyReply) => {
+server.decorate('authenticate', async (request: FastifyRequest, reply: FastifyReply) => {
     try {
         await request.jwtVerify()
     } catch (err) {
