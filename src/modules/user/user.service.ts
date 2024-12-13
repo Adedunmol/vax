cc451bbcda1a190f4d0dddd44b12910762f75b55
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2024-12-13 10:55:49 +0100

    add functions to create and find users

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,16 +1,22 @@
+import { eq } from 'drizzle-orm'
+import db from '../../db'
+import { users } from '../../db/schema'
 import { CreateUserInput } from './user.schema'
 import bcrypt from 'bcrypt'
 
 export async function createUser(input: CreateUserInput) {
-    // create user
+    const password = await bcrypt.hash(input.password, 10)
 
-    // hash password
+    const user = await db.insert(users).values({ ...input, password }).returning()
 
-    // return user
+    return user
 }
 
 export async function findUserByEmail(email: string) {
     // find and return user by email
+    const user = await db.select().from(users).where(eq(users.email, email))
+
+    return user
 }
 
 const OTP_EXPIRATION = 3600000
@@ -21,6 +27,8 @@ export async function generateOTP(id: number, email: string) {
     const otp = `${Math.floor(1000 + Math.random() * 9000)}`
 
     const hashedOTP = await bcrypt.hash(otp, 10)
+
+    const expiresAt = Date.now() + OTP_EXPIRATION
 
     // create an entry in the otp verification table
 
