b9d3efd0c8aa7efe4f85786dd3eccb350ecab803
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2024-12-13 10:46:29 +0100

    add sending of emails

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,4 +1,5 @@
-import { CreateUserInput } from "./user.schema";
+import { CreateUserInput } from './user.schema'
+import bcrypt from 'bcrypt'
 
 export async function createUser(input: CreateUserInput) {
     // create user
@@ -10,4 +11,18 @@ export async function createUser(input: CreateUserInput) {
 
 export async function findUserByEmail(email: string) {
     // find and return user by email
+}
+
+const OTP_EXPIRATION = 3600000
+
+
+export async function generateOTP(id: number, email: string) {
+
+    const otp = `${Math.floor(1000 + Math.random() * 9000)}`
+
+    const hashedOTP = await bcrypt.hash(otp, 10)
+
+    // create an entry in the otp verification table
+
+    return otp
 }
---
+++ src/utils/mail.ts
@@ -0,0 +1,37 @@
+import path from 'path'
+import Email from "email-templates"
+import { server } from ".."
+import env from '../env'
+
+
+export const sendMailWithTemplates = async (template: string, locals: any,  to: string, invoicePath?: string, invoiceId?: string) => {
+    try {
+        server.log.info('from send mail with templates: ', invoicePath)
+        const email = new Email({
+            message: {
+              from: env.EMAIL_FROM,
+              attachments: invoicePath ? [{ path: invoicePath, contentType: 'application/pdf', filename: `${invoiceId}.pdf`}] : []
+            },
+            send: true,
+            preview: false,
+            transport: {
+                host: env.EMAIL_HOST,
+                port: env.EMAIL_PORT,
+                auth: {
+                user: env.EMAIL_USERNAME,
+                pass: env.EMAIL_PASSWORD
+              }
+            },
+        })
+
+        await email.send({ 
+            template: path.join(__dirname, '..', 'emails', template), 
+            message: { to }, 
+            locals,
+        })
+        server.log.info(`email sent to ${to}`)
+    } catch (err: any) {
+        server.log.error(`unable send mail to: ${to}`)
+        server.log.error(err)
+    }
+}
