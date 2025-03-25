07cdeb7594a245edbdd1f9833df575eb0e0149a3
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2025-03-25 13:30:07 +0100

    fix bug

--- src/modules/settings/settings.service.ts
+++ src/modules/settings/settings.service.ts
@@ -1,18 +1,16 @@
 import { eq } from 'drizzle-orm'
 import db from '../../db'
-import { profiles, users } from '../../db/schema'
+import { users } from '../../db/schema'
 import settings from '../../db/schema/settings'
 
 class SettingsService {
-    OTP_EXPIRATION: number
 
     constructor() {
-        this.OTP_EXPIRATION = 3600000
     }
 
     async update(userId: number, updateObj: any) {
 
-        const data = await db.update(settings).set(updateObj).where(eq(users.id, userId)).returning()
+        const data = await db.update(settings).set(updateObj).where(eq(settings.userId, userId)).returning()
     
         return data[0]
     }
