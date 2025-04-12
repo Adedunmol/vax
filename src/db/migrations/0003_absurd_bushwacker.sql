7d2f7f3b33a636610f481acc589dbb2e7e2d27b9
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2025-04-12 11:19:21 +0100

    add migration for tables

---
+++ src/db/migrations/0003_absurd_bushwacker.sql
@@ -0,0 +1,111 @@
+CREATE TABLE "clients" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"first_name" varchar(50),
+	"last_name" varchar(50),
+	"email" varchar(64) NOT NULL,
+	"phone_number" varchar(16),
+	"created_by" integer,
+	"updated_at" timestamp,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"deleted_at" timestamp
+);
+--> statement-breakpoint
+CREATE TABLE "expenses" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"user_id" integer,
+	"category" varchar(64),
+	"amount" numeric,
+	"expenseDate" timestamp DEFAULT now(),
+	"updated_at" timestamp,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"deleted_at" timestamp
+);
+--> statement-breakpoint
+CREATE TABLE "invoices" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"description" varchar(255),
+	"due_date" timestamp,
+	"created_by" integer,
+	"created_for" integer,
+	"total_amount" numeric,
+	"amount_paid" numeric,
+	"status" "invoice_status" DEFAULT 'unpaid',
+	"updated_at" timestamp,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"deleted_at" timestamp
+);
+--> statement-breakpoint
+CREATE TABLE "items" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"invoice_id" integer NOT NULL,
+	"units" integer NOT NULL,
+	"description" varchar,
+	"rate" numeric,
+	"total" numeric
+);
+--> statement-breakpoint
+CREATE TABLE "payments" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"user_id" integer,
+	"invoice_id" integer,
+	"amount" numeric,
+	"payment_date" timestamp,
+	"payment_method" varchar,
+	"updated_at" timestamp,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"deleted_at" timestamp
+);
+--> statement-breakpoint
+CREATE TABLE "reminders" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"user_id" integer,
+	"client_id" integer,
+	"invoice_id" integer,
+	"reminderStatus" "reminder_status" DEFAULT 'pending',
+	"is_recurring" boolean DEFAULT false,
+	"interval_days" integer DEFAULT 1,
+	"due_date" timestamp NOT NULL,
+	"canceled" boolean DEFAULT false,
+	"updated_at" timestamp,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"deleted_at" timestamp
+);
+--> statement-breakpoint
+CREATE TABLE "settings" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"user_id" integer,
+	"currency" varchar(3),
+	"custom_logo" varchar(255),
+	"notify_before" integer DEFAULT 0,
+	"recurrent_reminders" boolean DEFAULT true,
+	"recurrent_interval" integer DEFAULT 0,
+	"lastLogin" timestamp DEFAULT now(),
+	"updated_at" timestamp,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"deleted_at" timestamp,
+	CONSTRAINT "settings_user_id_unique" UNIQUE("user_id")
+);
+--> statement-breakpoint
+CREATE TABLE "user_otp_verifications" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"user_id" integer NOT NULL,
+	"otp" varchar(6) NOT NULL,
+	"expires_at" integer NOT NULL,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
+	"updated_at" timestamp with time zone DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "username" varchar(50) NOT NULL;--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "refresh_token" varchar(255);--> statement-breakpoint
+ALTER TABLE "clients" ADD CONSTRAINT "clients_created_by_users_id_fk" FOREIGN KEY ("created_by") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "expenses" ADD CONSTRAINT "expenses_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "invoices" ADD CONSTRAINT "invoices_created_by_users_id_fk" FOREIGN KEY ("created_by") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "invoices" ADD CONSTRAINT "invoices_created_for_clients_id_fk" FOREIGN KEY ("created_for") REFERENCES "public"."clients"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "items" ADD CONSTRAINT "items_invoice_id_invoices_id_fk" FOREIGN KEY ("invoice_id") REFERENCES "public"."invoices"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "payments" ADD CONSTRAINT "payments_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "payments" ADD CONSTRAINT "payments_invoice_id_invoices_id_fk" FOREIGN KEY ("invoice_id") REFERENCES "public"."invoices"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "reminders" ADD CONSTRAINT "reminders_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "reminders" ADD CONSTRAINT "reminders_client_id_clients_id_fk" FOREIGN KEY ("client_id") REFERENCES "public"."clients"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "reminders" ADD CONSTRAINT "reminders_invoice_id_invoices_id_fk" FOREIGN KEY ("invoice_id") REFERENCES "public"."invoices"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "settings" ADD CONSTRAINT "settings_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "user_otp_verifications" ADD CONSTRAINT "user_otp_verifications_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
