250056ea9e510805f15ecfb403fbe9231ad4ac6e
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2025-04-21 14:41:48 +0100

    cache analytics responses

--- src/modules/analytics/analytics.controller.ts
+++ src/modules/analytics/analytics.controller.ts
@@ -7,25 +7,67 @@ export async function revenueHandler(request: FastifyRequest<{ Querystring: Reve
         const userId = request.user.id
 
         let revenue: object
+        let cachedRevenue: string | null
 
         switch (request.query.type) {
             case 'total':
+                cachedRevenue = await request.redis.get(`cache:total-revenue:${userId}`)
+
+                if (cachedRevenue) return reply.code(200).send({ status: 'success', message: "Revenue retrieved successfully", data: JSON.parse(cachedRevenue) })
+
                 revenue = await revenueAnalytics.totalRevenue(userId)
+
+                await request.redis.set(`cache:total-revenue:${userId}`, JSON.stringify(revenue))
                 break
             case 'monthly':
+                cachedRevenue = await request.redis.get(`cache:monthly-revenue:${userId}`)
+
+                if (cachedRevenue) return reply.code(200).send({ status: 'success', message: "Revenue retrieved successfully", data: JSON.parse(cachedRevenue) })
+
                 revenue = await revenueAnalytics.monthlyRevenue(userId)
+
+                await request.redis.set(`cache:monthly-revenue:${userId}`, JSON.stringify(revenue))
+
                 break
             case 'outstanding':
+                cachedRevenue = await request.redis.get(`cache:outstanding-revenue:${userId}`)
+
+                if (cachedRevenue) return reply.code(200).send({ status: 'success', message: "Revenue retrieved successfully", data: JSON.parse(cachedRevenue) })
+
                 revenue = await revenueAnalytics.outstandingRevenue(userId)
+
+                await request.redis.set(`cache:outstanding-revenue:${userId}`, JSON.stringify(revenue))
+
                 break
             case 'average':
+                cachedRevenue = await request.redis.get(`cache:average-revenue:${userId}`)
+
+                if (cachedRevenue) return reply.code(200).send({ status: 'success', message: "Revenue retrieved successfully", data: JSON.parse(cachedRevenue) })
+
                 revenue = await revenueAnalytics.averageRevenue(userId)
+
+                await request.redis.set(`cache:average-revenue:${userId}`, JSON.stringify(revenue))
+
                 break
             case 'top-clients':
+                cachedRevenue = await request.redis.get(`cache:clients-revenue:${userId}`)
+
+                if (cachedRevenue) return reply.code(200).send({ status: 'success', message: "Revenue retrieved successfully", data: JSON.parse(cachedRevenue) })
+
                 revenue = await revenueAnalytics.topClientsRevenue(userId)
+
+                await request.redis.set(`cache:clients-revenue:${userId}`, JSON.stringify(revenue))
+
                 break
             case 'payment-method':
+                cachedRevenue = await request.redis.get(`cache:payments-revenue:${userId}`)
+
+                if (cachedRevenue) return reply.code(200).send({ status: 'success', message: "Revenue retrieved successfully", data: JSON.parse(cachedRevenue) })
+
                 revenue = await revenueAnalytics.paymentMethodsRevenue(userId)
+
+                await request.redis.set(`cache:payments-revenue:${userId}`, JSON.stringify(revenue))
+
                 break
             default:
                 return reply.code(400).send({ status: 'error', message: 'Revenue type is unknown' })
